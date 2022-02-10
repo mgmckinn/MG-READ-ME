dd4e20faa761a3dd73560d745d6181850351ea3c
Mitch McKinney <mgmckinn2325@me.com>
2022-02-09 21:52:58 -0500

    update spacing on markdown

---
+++ Develop/ReadMe.md
@@ -0,0 +1,41 @@
+
+<h1 align="center font-size=20px font-weight=bold">fdgdafgadfsgadfgdaf</h1>
+
+## Description
+dfgaagadfgadfg
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributors](#contributors)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation adfgadfgadsfgadsf
+
+## Usage
+dsafgadsfgadfgadfg
+
+# License
+![badge](https://img.shields.io/badge/license-ISC-brightgreen)
+<br />
+This application is covered by the ISC License. 
+
+## Contributors
+ sadgasdgasdgads
+
+## Tests
+sdgsadgasdgasdg
+
+## Questions
+ asdgasdgasdgasdg<br />
+<br />
+
+Find me on GitHub: [undefined](https://github.com/undefined)<br />
+<br />
+
+Email me with any questions: undefined<br /><br />
+
+This README was generated with by [MG-READ-ME](https://github.com/mgmckinn/MG-READ-ME)
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,40 +2,39 @@
 // TODO:  
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license !== null) {
-    return `![badge](https://img.shields.io/badge/license-${data.license}-brightorange)`
-  }
-  return ""
+if(license !== null) {
+return `![badge](https://img.shields.io/badge/license-${data.license}-brightorange)`
+}
+return ""
 }
   
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license !== null) {
-    return '\n* [license](#license)\n'
-  }
+if(license !== null) {
+return '\n* [license](#license)\n'
+}
 }
 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(license !== null) {
-    return(`## license
-    this repository is licensed under ${license}license`)
-  }
+if(license !== null) {
+return(`## License
+This repository is licensed under the ${license} license`)
+}
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # ${data.title}
-  ${data.GitHub}
-  ${renderLicenseSection(data.license)}
-  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
+return `
+<h1 align="center font-size=20px font-weight=bold">${data.title}</h1>
+
 ## Description
 ${data.description}
+
 ## Table of Contents
 - [Description](#description)
 - [Installation](#installation)
@@ -44,27 +43,33 @@ ${data.description}
 - [Contributors](#contributors)
 - [Tests](#tests)
 - [Questions](#questions)
-## Installation
- ${data.installation}
+
+## Installation ${data.installation}
+
 ## Usage
 ${data.usage}
-## License
+
+# License
 ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
 <br />
-This application is covered by the ${data.license} license. 
-## contributors
+This application is covered by the ${data.license} License. 
+
+## Contributors
  ${data.contributors}
+
 ## Tests
 ${data.tests}
+
 ## Questions
  ${data.questions}<br />
 <br />
-:octocat: Find me on GitHub: [${data.username}](https://github.com/${data.username})<br />
-<br />
-✉️ Email me with any questions: ${data.email}<br /><br />
 
-_This README was generated with ❤️ by [MG-READ-ME](https://github.com/mgmckinn/MG-READ-ME)
-`;
+Find me on GitHub: [${data.username}](https://github.com/${data.username})<br />
+<br />
+
+Email me with any questions: ${data.email}<br /><br />
+
+This README was generated with by [MG-READ-ME](https://github.com/mgmckinn/MG-READ-ME)`;
   };
 
-module.exports = generateMarkdown;
+  module.exports = generateMarkdown;
